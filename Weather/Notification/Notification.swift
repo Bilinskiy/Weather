4ca6556ce41c2333a2c4075cc16f2b9d09a2e752
Bilinskiy <bilinskijwork@gmail.com>
2024-12-12 13:15:15 +0300

    add local notification

---
+++ Weather/Notification/Notification.swift
@@ -0,0 +1,71 @@
+//
+//  Notification.swift
+//  Weather
+//
+//  Created by Дмитрий Билинский on 12/3/24.
+//
+
+import Foundation
+import UIKit
+
+protocol NotificationProtokol {
+  func notification(data: [Hourly])
+}
+
+class Notification: NotificationProtokol {
+  let notificationCenter = UNUserNotificationCenter.current()
+
+  func addNotification(data: Hourly) {
+    notificationCenter.requestAuthorization(options: [.alert, .sound]) { isAuthorized, error in
+      if isAuthorized {
+        
+        let content = UNMutableNotificationContent()
+        
+        guard let dt = data.dt, let description = data.weather.first?.description else {return}
+        
+        content.body = description
+        
+        let dateTimeInterval = Date(timeIntervalSince1970: TimeInterval(dt))
+        
+        let calendar = Calendar.current
+        
+        guard let date = calendar.date(byAdding: .minute, value: -30, to: dateTimeInterval) else {return}
+        
+        let components = calendar.dateComponents([.day, .hour, .minute], from: date)
+     
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+        
+        let identifier = "identifier\(data)"
+        
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        self.notificationCenter.add(request)
+        print (request)
+      }
+    }
+  }
+    
+    func notification(data: [Hourly]) {
+      notificationCenter.removeAllDeliveredNotifications()
+      
+      let filter200 = data.filter({(200..<300).contains($0.weather.first?.id ?? 0)})
+      let filter500 = data.filter({(500..<600).contains($0.weather.first?.id ?? 0)})
+      let filter600 = data.filter({(600..<700).contains($0.weather.first?.id ?? 0)})
+
+      if !filter200.isEmpty { sortNotification(data: filter200) }
+      if !filter500.isEmpty { sortNotification(data: filter500) }
+      if !filter600.isEmpty { sortNotification(data: filter600) }
+    }
+  
+  func sortNotification(data: [Hourly]) {
+    for i in 0 ..< data.count {
+      if  i != 0 {
+        if data[i].dt! - data[i-1].dt! != 3600.0  { addNotification(data: data[i]) }
+      } else {
+        addNotification(data: data[i])
+      }
+    }
+  }
+  
+
+}
